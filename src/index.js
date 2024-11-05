94843c9ec21d614dc73c25d7ed01c693c1cdb4c1
Jerry Huy <dnhuy04.it@gmail.com>
2024-11-05 19:32:00 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const port = 3000;
 const { engine } = require('express-handlebars');
 const path = require('path');
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -21,22 +23,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.render('search');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
