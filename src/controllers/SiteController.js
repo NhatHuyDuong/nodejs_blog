2a5a409c585d93d32d4bce6fb5fa67c71315d768
Jerry Huy <dnhuy04.it@gmail.com>
2024-11-08 19:58:00 +0700

    Create Courses

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,15 +1,14 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../util/mongoose');
 
 class SiteController {
     // [GET] /
-    async index(req, res) {
-        // res.render('home');
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (error) {
-            res.status(400).json({ error: 'ERROR!!!' });
-        }
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', { courses: multipleMongooseToObject(courses) });
+            })
+            .catch(next);
     }
 
     // [GET] /search
